85689858351924d31a0777692930b0b4983c1c22
ture1996 <turanjanin1996@gmail.com>
2022-12-08 21:45:39 +0100

    Fourth commit

--- app/Http/Controllers/PlayersController.php
+++ app/Http/Controllers/PlayersController.php
@@ -8,6 +8,11 @@ use Illuminate\Http\Request;
 
 class PlayersController extends Controller
 {
+
+    public function __construct(){
+        $this->middleware('auth');
+    }
+
     public function show($id){
 
         $player = Player::find($id);
--- app/Http/Controllers/TeamsController.php
+++ app/Http/Controllers/TeamsController.php
@@ -7,6 +7,11 @@ use App\Models\Team;
 
 class TeamsController extends Controller
 {
+
+    public function __construct(){
+        $this->middleware('auth');
+    }
+
     public function index(){
         $teams = Team::all();
 
